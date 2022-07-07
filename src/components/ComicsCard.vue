c66700ac540a57581cda499c54d8bbf34d453f8d
E-Lop <edmondolopez@gmail.com>
2022-07-07 12:16:05 +0200

    fumetti stampati dinamicamente

---
+++ src/components/ComicsCard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="comic_book_card">
+    <img :src="comicsDetails.thumb" alt="comicsDetails.series" />
+    <h2>{{ comicsDetails.series }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ComicsCard',
+  props: {
+    comicsDetails: Object,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.comic_book_card {
+  padding: 10px;
+  width: calc((100% / 6) - 5px);
+  margin: 2px;
+  //   todo test
+  background-color: white;
+}
+</style>
